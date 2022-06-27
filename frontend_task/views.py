e5f3df671b5eb78fc8c80f9f8a44d72aff0a87fd
rkrvchnk <nwrkrvchnk@gmail.com>
2022-06-27 11:51:39 -0400

    add templates

--- frontend_task/views.py
+++ frontend_task/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render
 
 class Counter:
     count = 0
-    def __str__(self):
-        result = self.count
-        result = "count = " + str(result)
-        return result
+
 
 
 rollups_clicks = Counter()
@@ -17,5 +14,6 @@ def index(request):
 
 
 def rollups(request):
+
     rollups_clicks.count += 1
     return render(request, 'frontend_task/rollups.html', {'count': rollups_clicks.count})

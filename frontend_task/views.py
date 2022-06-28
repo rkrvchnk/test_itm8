e53360f51de1d9ce7ba5c4169fc971cc0fe050a6
rkrvchnk <nwrkrvchnk@gmail.com>
2022-06-28 12:12:14 -0400

    add a counter and reset it

--- frontend_task/views.py
+++ frontend_task/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 
 
-class Counter:
-    count = 0
-
-
-
-rollups_clicks = Counter()
-
-
 def index(request):
     return render(request, 'frontend_task/index.html', {})
 
 
 def rollups(request):
-
-    rollups_clicks.count += 1
-    return render(request, 'frontend_task/rollups.html', {'count': rollups_clicks.count})
+    return render(request, 'frontend_task/rollups.html', {})
